f787eb507eecf790daac47e5f33742136f76060c
ilovetyfng <enderkiller3472@gmail.com>
2022-02-09 22:16:49 +0800

    Add number button function

--- index.js
+++ index.js
@@ -26,3 +26,49 @@ function operate(op, a, b) {
             divide(a, b);
     }
 }
+
+function addUserInput(str) {
+    switch (str) {
+        case '1':
+            addDisplay(1);
+            break;
+        case '2':
+            addDisplay(2);
+            break;
+        case '3':
+            addDisplay(3);
+            break;
+        case '4':
+            addDisplay(4);
+            break;
+        case '5':
+            addDisplay(5);
+            break;
+        case '6':
+            addDisplay(6);
+            break;
+        case '7':
+            addDisplay(7);
+            break;
+        case '8':
+            addDisplay(8);
+            break;
+        case '9':
+            addDisplay(9);
+            break;
+    }
+}
+
+function addDisplay(str) {
+    calcDisplay += str;
+    displayDiv.textContent = calcDisplay;
+}
+
+let calcDisplay = "";
+const btns = document.querySelectorAll('button');
+const displayDiv = document.querySelector('.display')
+btns.forEach(
+    btn => btn.addEventListener(
+        'click', btn => addUserInput(btn.target.getAttribute('data-btn'))
+        )
+)
